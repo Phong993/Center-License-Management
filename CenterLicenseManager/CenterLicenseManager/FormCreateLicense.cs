6f790a212ad5dad1387ea530b509ffeb078a6699
PhongBIM <iphong993@gmail.com>
2017-06-05 14:04:30 +0700

    New Update: 05/06/2017

--- CenterLicenseManager/CenterLicenseManager/FormCreateLicense.cs
+++ CenterLicenseManager/CenterLicenseManager/FormCreateLicense.cs
@@ -201,15 +201,17 @@ namespace CenterLicenseManager
         {
             this.mAddinNameList = new Dictionary<string, string>();
 
-            this.mAddinNameList.Add("d578b210-3fa4-4528-9a6a-6ca60bbd51d3", "Rebar Detailing");
-            this.mAddinNameList.Add("cbbd3d14-d9f6-454f-8349-3237847ccfd4", "Sheet Maker - Duplicate View - Align View");
-            this.mAddinNameList.Add("e6ffda32-58f8-4e88-b19c-8123bf45e2fa", "Formwork Area");
-            this.mAddinNameList.Add("483ada3a-a4ff-4f59-9c71-4189d0dfffcc", "Export/Import Schedule To/From Excel");
+            //this.mAddinNameList.Add("d578b210-3fa4-4528-9a6a-6ca60bbd51d3", "Rebar Detailing");
+            //this.mAddinNameList.Add("cbbd3d14-d9f6-454f-8349-3237847ccfd4", "Sheet Maker - Duplicate View - Align View");
+            //this.mAddinNameList.Add("e6ffda32-58f8-4e88-b19c-8123bf45e2fa", "Formwork Area");
+            //this.mAddinNameList.Add("483ada3a-a4ff-4f59-9c71-4189d0dfffcc", "Export/Import Schedule To/From Excel");
             this.mAddinNameList.Add("18bca09b-da16-4171-a955-7692d0e3713f", "Element Locate 2017");
             this.mAddinNameList.Add("647fb033-66e1-45c9-8ea7-4e7c7cc00692", "TotalRebar - MutipleSelect 2017");
             this.mAddinNameList.Add("e4f0facc-6828-43a1-9213-30ae72839e35", "AutoJoinElements");
             this.mAddinNameList.Add("5045b6d9-899f-4ec0-9fa8-e672aa8dd966", "AddinBimHoaBinh");
-        }
+            this.mAddinNameList.Add("a277ca6b-1b46-476e-8372-2bbf7e31ff8d", "Tile Count - Write");
+			this.mAddinNameList.Add("4b5163b6-7d43-44ba-9f2f-16f4096eebda", "RoomAssign_2017");
+		}
 
         private void createVersionList()
         {
